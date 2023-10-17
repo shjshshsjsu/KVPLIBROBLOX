74035977ad6309ad02dfed6b2bea2ffc3ff250cb
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-17 20:37:28 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,6 +7,11 @@ local guiFrame = MyGUI:CreateFrame("Kvp Lib", UDim2.new(0, 300, 0, 200))
 local guiText = Instance.new("TextLabel")
 guiText.Parent = guiFrame
 guiText.Size = UDim2.new(1, 0, 1, 0)
-guiText.Text = ""
+guiText.Text = "Kvp Lib"
 guiText.TextColor3 = Color3.new(1, 1, 1)
 guiText.BackgroundTransparency = 1 -- Arka plan şeffaf
+
+MyGUI:CreateButton(guiFrame, "Butona Tıkla", function()
+    print("Butona tıklandı!")
+    -- İstediğiniz ek işlemleri burada gerçekleştirebilirsiniz
+end)
