50f917e0293a515419db0c0ca8429698670201d9
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-17 20:35:11 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,12 @@
 local MyGUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/shjshshsjsu/KVPLIBROBLOX/main/Lib.lua"))()
 
--- GUI çerçevesini oluştur
-local guiFrame = MyGUI:CreateFrame("Siyah Temalı GUI", UDim2.new(0, 300, 0, 200))
 
--- GUI'ye bir metin ekleyebilirsiniz
+local guiFrame = MyGUI:CreateFrame("Kvp Lib", UDim2.new(0, 300, 0, 200))
+
+
 local guiText = Instance.new("TextLabel")
 guiText.Parent = guiFrame
 guiText.Size = UDim2.new(1, 0, 1, 0)
-guiText.Text = "Merhaba, bu siyah temalı GUI!"
+guiText.Text = ""
 guiText.TextColor3 = Color3.new(1, 1, 1)
 guiText.BackgroundTransparency = 1 -- Arka plan şeffaf
